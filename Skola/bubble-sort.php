1526996051bc767b41bf1518cec38dc0a8cf5820
simakrejci <simon.krejci@educanet.cz>
2023-01-04 14:14:37 +0100

    ukol skola

--- Skola/bubble-sort.php
+++ Skola/bubble-sort.php
@@ -1,1 +1,20 @@
 <?php
+
+function bubble_sort($array) {
+    $n = count($array);
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $tmp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $tmp;
+            }
+        }
+    }
+    return $array;
+}
+
+$array = [5, 2, 4, 1, 3];
+print_r(bubble_sort($array));
+
+?>
--- Skola/selection-sort.php
+++ Skola/selection-sort.php
@@ -1,1 +1,25 @@
 <?php
+function selection_sort($array) {
+    $n = count($array);
+    for ($i = 0; $i < $n; $i++) {
+        $min = $i;
+        for ($j = $i + 1; $j < $n; $j++) {
+            if ($array[$j] < $array[$min]) {
+                $min = $j;
+            }
+        }
+        if ($min != $i) {
+            $tmp = $array[$i];
+            $array[$i] = $array[$min];
+            $array[$min] = $tmp;
+        }
+    }
+    return $array;
+}
+
+$array = [5, 2, 4, 1, 3];
+print_r(selection_sort($array));
+
+
+
+?>
