7a82782bb12999c6a875517f6d4f9110ebe5b218
simakrejci <simon.krejci@educanet.cz>
2023-01-11 08:48:03 +0100

    yes

--- 2/rozmenovani.php
+++ 2/rozmenovani.php
@@ -1,3 +1,18 @@
 <?php
+$castka = 3000;
+
+$bankovky = array(5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1);
+
+$output = array();
+foreach ($bankovky as $denomination) {
+    if ($bankovky <= $castka) {
+        $output[$bankovky] = floor($castka / $bankovky);
+        $castka = $castka % $bankovky;
+    }
+}
+
+foreach ($output as $bankovky => $count) {
+    echo $count . 'x ' . $bankovky . ' Kč' . PHP_EOL;
+}
 
 ?>
