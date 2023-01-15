a6e2570ff24ef5d3f91bea32449935c8dd39cb2b
simakrejci <simon.krejci@educanet.cz>
2023-01-15 21:53:25 +0100

    .

---
+++ 2/KontrolaFormulare.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<div class="formular">
+    <form method="post">
+        <div class="textove_pole">
+            <input type="text" required>
+            <span></span>
+            <label>Uživatelské jméno</label>
+        </div>
+        <div class="textove_pole">
+            <input type="password" required>
+            <span></span>
+            <label>Heslo</label>
+        </div>
+        <div class="textove_pole">
+            <input type="text" required>
+            <span></span>
+            <label>adresa</label>
+        </div>
+
+
+        </div>
+
+    </form>
+
+
+</div>
+</body>
+</html>
+
+
+
+<?php
+
+
+?>
