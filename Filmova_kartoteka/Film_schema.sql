58af94bba5f83e1c1ce3007cc71f40ab6c53916f
simakrejci <simon.krejci@educanet.cz>
2023-03-23 21:15:50 +0100

    number 2

--- Filmova_kartoteka/Film_schema.sql
+++ Filmova_kartoteka/Film_schema.sql
@@ -48,3 +48,32 @@ CREATE TABLE Film_Oceneni (
     FOREIGN KEY (FilmID) REFERENCES Film(ID),
     FOREIGN KEY (OceneniID) REFERENCES Oceneni(ID)
 );
+
+
+ALTER TABLE Film ADD CONSTRAINT FK_Film_Kategorie FOREIGN KEY (ID_kategorie) REFERENCES Kategorie(ID);
+
+CREATE FUNCTION NejdelsiFilmVKategorii (@kategorie nvarchar(50))
+RETURNS int
+AS
+BEGIN
+DECLARE @delka int;
+SELECT TOP 1 @delka = Delka
+FROM Film
+WHERE ID_kategorie = (SELECT ID FROM Kategorie WHERE Nazev = @kategorie)
+ORDER BY Delka DESC;
+RETURN @delka;
+END;
+
+CREATE PROCEDURE VymazaniKategoriiBezFilmu
+AS
+BEGIN
+DELETE FROM Kategorie
+WHERE NOT EXISTS (SELECT 1 FROM Film WHERE ID_kategorie = Kategorie.ID);
+END;
+
+CREATE VIEW Film_Herci AS
+SELECT f.Nazev, COUNT(fh.ID_herec) AS Pocet_hercu
+FROM Film f
+LEFT JOIN Film_Herec fh ON f.ID = fh.ID_film
+GROUP BY f.ID;
+
